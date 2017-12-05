561864314258d6b6950ce1c0a250e53b14da1da5
herwindra <herwin@windowslive.com>
2017-12-05 12:58:42 +0000

    Revert "first commit"
    This reverts commit 57b609e40ed2c1d4d831192735afc42dc4a7cdab

--- node_modules/native-base/src/Utils/index.js
+++
@@ -1,3 +0,0 @@
-import ViewPropTypes from './viewPropTypes'
-
-export { ViewPropTypes }
--- node_modules/react-native-buttons/judge.js
+++
@@ -1,8 +0,0 @@
-import {Platform} from 'react-native';
-
-export function judgePlatformLevel(module) {
-  const level = Platform.Version;
-  if (module === 'TouchableNativeFeedback') {
-    return level >= 21;
-  }
-}
